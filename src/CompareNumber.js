0699540d1e80f997323c2c1bb264826b0fc52d59
余泽江 <yzj1995@vip.qq.com>
2014-08-13 20:35:28 +0800

    重构第一问实现

--- src/CompareNumber.js
+++ src/CompareNumber.js
@@ -3,28 +3,23 @@ function CompareNumber() {
 }
 
 CompareNumber.prototype.checkGuess = function(answer, guess, set) {
-  var count_A = 0;
-  var count_total = 0;
+  var count_A = count_total = 0;
   var set = set || {};
   var compare_A = {};
   var compare_total = {};
   this.reduce = 0;
   for (var i = 3; i >= 0; i--) {
-  	count_A += compare_A[answer[i].concat(i)]? 1: 0;
  	  compare_A[answer[i].concat(i)] = true;
-  	count_A += compare_A[guess[i].concat(i)]? 1: 0;
- 	  compare_A[guess[i].concat(i)] = true;
+  	compare_total[answer[i]] = true;
+  };
 
+  for (var i = 3; i >= 0; i--) {
+    count_A += compare_A[guess[i].concat(i)]? 1: 0;
+    count_total += compare_total[guess[i]]? 1: 0;
     if(set[guess[i].concat(i)]) {
       this.reduce += 1;
       set[guess[i].concat(i)] = false; 
     }
-
-  	count_total += compare_total[answer[i]]? 1: 0;
-  	compare_total[answer[i]] = true;
-  	count_total += compare_total[guess[i]]? 1: 0;
-  	compare_total[guess[i]] = true;
-
   };
   count_B = count_total - count_A;
   return "".concat(count_A).concat("A").concat(count_B).concat("B");
